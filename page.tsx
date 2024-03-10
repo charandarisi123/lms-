5d9d1c4ae582e0c53e54223bf846c53212b7f92e
charandarisi123 <162859573+charandarisi123@users.noreply.github.com>
2024-03-10 11:01:22 +0530

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,34 @@
+'use client'
+import React, {FC,useState} from "react";
+import Heading from "./utils/Heading";
+import Header from "./components/Header";
+import Hero from "./components/Route/Hero";
+
+
+interface Props{}
+
+const Page: FC<Props> = (props) => {
+  const[open,setOpen] = useState(false);
+  const[activeItem,setActiveItem] = useState(0);
+  const [route,setRoute] = useState("Login");
+
+  return (
+    <div>
+      <Heading
+        title="Student Learning Management System"
+        description="LMS is a platform for students to learn and get help from teachers"
+        keywords="Programing,MERN,Machine Learing,Python"
+      />
+      <Header
+        open={open}
+        setOpen={setOpen}
+        activeItem={activeItem}
+        setRoute = {setRoute}
+        route={route}
+      />
+      <Hero />
+    </div>
+  );
+};
+
+export default Page;
